5dcfe27949706aa63aeb46000423d9e60da794fb
DESKTOP-BMQPELL\claudio <claudiolaguzzi@gmail.com>
2024-10-28 23:31:03 -0300

    prueba

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
         to="/home"
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
-        INGRESAR 
+        INGRESAR AL SITIO
       </Link>
     </div>
   );
