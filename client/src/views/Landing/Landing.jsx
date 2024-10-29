ceec58c07e91d700b1c4d5aa763ecf794bcc5b26
DESKTOP-BMQPELL\claudio <claudiolaguzzi@gmail.com>
2024-10-28 23:28:54 -0300

    first commit

---
+++ client/src/views/Landing/Landing.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold mb-6">Landing Page</h1>
+      <Link
+        to="/home"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        INGRESAR 
+      </Link>
+    </div>
+  );
+};
+
+export default Landing;
